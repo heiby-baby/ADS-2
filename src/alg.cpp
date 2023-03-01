0d0bba48ba7708601aed1c35fca86e502f954e0b
Кандрушин Максим 22-IST-1 <125025831+heiby-baby@users.noreply.github.com>
2023-03-01 22:41:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,49 +5,49 @@
 
 double pown(double value, uint16_t n) {
     if (0 == n) {
-		return 1;
-	} else {
-		double num = 1;
-		for (int i = 1; i <= n; i++) {
-			num *= value;
-		}
-		return num;
-	}
+        return 1;
+    } else {
+        double num = 1;
+        for (int i = 1; i <= n; i++) {
+            num *= value;
+    }
+        return num;
+    }
 }
 
 uint64_t fact(uint16_t n) {
     uint64_t num = 1;
-	for (uint16_t i = n; i > 0; i--) {
-		num *= i;
-	}
-	return num;
+    for (uint16_t i = n; i > 0; i--) {
+        num *= i;
+    }
+    return num;
 }
 
 double calcItem(double x, uint16_t n) {
     double num = pown(x, n) / fact(n);
-	return num;
+    return num;
 }
 
 double expn(double x, uint16_t count) {
     double exponenta = 1;
-	for (int i = 1; i <= count; i++) {
-		exponenta += calcitem(x, i);
-	}
-	return exponenta;
+    for (int i = 1; i <= count; i++) {
+        exponenta += calcItem(x, i);
+    }
+    return exponenta;
 }
 
 double sinn(double x, uint16_t count) {
     double sinus = 0;
-	for (int i = 1; i <= count; i++) {
-		sinus += pown(-1, i-1) * calcitem(x, 2*i-1);
-	}
-	return sinus;
+    for (int i = 1; i <= count; i++) {
+        sinus += pown(-1, i-1) * calcItem(x, 2*i-1);
+    }
+    return sinus;
 }
 
 double cosn(double x, uint16_t count) {
     double cosine = 0;
-	for (int i = 1; i <= count; i++) {
-		cosine += pown(-1, i - 1) * calcitem(x, 2 * i - 2);
-	}
-	return cosine;
+    for (int i = 1; i <= count; i++) {
+        cosine += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+    }
+    return cosine;
 }
